d41079a97569509c4975c5e255bd57ba831b9404
jeremy_shao <jeremy.shao@pwc.com>
2016-08-16 17:41:03 +0800

    integrated with frontend

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,7 @@ class AuthController < ApplicationController
   skip_before_action :authenticated_request
 
   def authenticate
-    user = User.find_by_credentials(params[:username], params[:password])
+    user = User.find_by_credentials(params[:userName], params[:password])
     if user
       render json: { auth_token: user.generate_auth_token, user: {name: user.name} }
     else 
