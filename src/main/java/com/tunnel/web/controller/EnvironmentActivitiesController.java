dccde11acebce622c9ee975bc11b7f131505c5a7
Wayne Cao <wayne911@live.cn>
2016-08-25 01:23:47 +0800

    add api to create activity

--- src/main/java/com/tunnel/web/controller/EnvironmentActivitiesController.java
+++ src/main/java/com/tunnel/web/controller/EnvironmentActivitiesController.java
@@ -1,7 +1,11 @@
 package com.tunnel.web.controller;
 
+import com.tunnel.model.EnvironmentActitivitySummary;
 import com.tunnel.model.EnvironmentActivity;
+import com.tunnel.repository.EnvironmentActitivitySummaryRepo;
 import com.tunnel.repository.EnvironmentActivityRepo;
+import com.tunnel.service.EnvironmentActitivitySummaryService;
+import com.tunnel.vo.CreateEnvironmentActitivitySummaryReqVo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -21,31 +25,54 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
-@RequestMapping("/environment-activities")
+@RequestMapping()
 public class EnvironmentActivitiesController extends BaseController {
 
 	@Autowired
 	private EnvironmentActivityRepo environmentActivityRepo;
+	
+	@Autowired
+	private EnvironmentActitivitySummaryRepo environmentActitivitySummaryRepo;
 
-	@RequestMapping(value = "/list/", method = RequestMethod.GET)
+	@Autowired
+	private EnvironmentActitivitySummaryService environmentActitivitySummaryService;
+
+	@RequestMapping(value = "/createEnvironmentActivity/", method = RequestMethod.POST)
 	@ResponseBody
-	public Page<EnvironmentActivity> get(
+	public ResponseEntity<CreateEnvironmentActitivitySummaryReqVo> createEnvironmentActivity(
+			@RequestBody CreateEnvironmentActitivitySummaryReqVo vo) {
+		log.info("create a new environment activities summary");
+		return new ResponseEntity<>(environmentActitivitySummaryService.createEnvironmentActitivitySummary(vo),
+				HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/environment-activities/list/", method = RequestMethod.GET)
+	@ResponseBody
+	public Page<EnvironmentActivity> getEnvironmentActivityPage(
 			@PageableDefault(value = 10, sort = { "id" }, direction = Direction.DESC) Pageable pageable) {
 		return environmentActivityRepo.findAll(pageable);
 	}
 
-	@RequestMapping(value = "/create/", method = RequestMethod.POST)
+	@RequestMapping(value = "/environment-activities/create/", method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<EnvironmentActivity> create(@RequestBody EnvironmentActivity environmentActivity) {
+	public ResponseEntity<EnvironmentActivity> createEnvironmentActivityDetail(@RequestBody EnvironmentActivity environmentActivity) {
 		return new ResponseEntity<>(environmentActivityRepo.save(environmentActivity), HttpStatus.OK);
 	}
 
 
-	@RequestMapping(value = "/listByActNo/{actNo}", method = RequestMethod.GET)
+	@RequestMapping(value = "/environment-activities/listByActNo/{actNo}", method = RequestMethod.GET)
 	@ResponseBody
 	public Page<EnvironmentActivity> get(
 			@PageableDefault(value = 10, sort = { "id" }, direction = Direction.DESC) Pageable pageable,
 			@PathVariable("actNo") String actNo) {
 		return environmentActivityRepo.findAllByActNo(actNo, pageable);
 	}
+	
+	@RequestMapping(value = "/environment-activities-summary/list/", method = RequestMethod.GET)
+	@ResponseBody
+	public Page<EnvironmentActitivitySummary> getEnvironmentActitivitySummaryPage(
+			@PageableDefault(value = 10, sort = { "id" }, direction = Direction.DESC) Pageable pageable) {
+		return environmentActitivitySummaryRepo.findAll(pageable);
+	}
+
 }
