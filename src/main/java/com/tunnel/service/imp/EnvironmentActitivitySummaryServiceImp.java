9823d32f391c3b0f97d015b747f15edd1ee6c651
Wayne Cao <wayne911@live.cn>
2016-08-26 23:05:19 +0800

    fix NPE

--- src/main/java/com/tunnel/service/imp/EnvironmentActitivitySummaryServiceImp.java
+++ src/main/java/com/tunnel/service/imp/EnvironmentActitivitySummaryServiceImp.java
@@ -16,7 +16,6 @@ import com.tunnel.repository.EnvironmentActivityRepo;
 import com.tunnel.repository.SurrInfoRepo;
 import com.tunnel.repository.EnvironmentActitivitySummaryRepo;
 
-
 import lombok.extern.slf4j.Slf4j;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
@@ -29,45 +28,44 @@ public class EnvironmentActitivitySummaryServiceImp implements EnvironmentActiti
 
 	@Autowired
 	private EnvironmentActivityRepo environmentActitivityRepo;
-	
+
 	@Autowired
 	private SurrInfoRepo surrInfoRepo;
-	
+
 	private DateFormat format = new SimpleDateFormat("yyyyMMdd");
-	
+
 	@Override
 	@Transactional
 	public CreateEnvironmentActitivitySummaryReqVo createEnvironmentActitivitySummary(
 			CreateEnvironmentActitivitySummaryReqVo vo) {
 		log.info("creating new activity ...");
-		
+
 		TSurrActSum activitySummary = vo.getEnvironmentActitivitySummary();
 		TSurrAct activity = vo.getEnvironmentActivity();
-		
+
 		/**
-		 * logic to generate act_no
-		 * 编码规则：活动类型（1位）+日期（首次发现活动的日期）8位+序号（2位）
+		 * logic to generate act_no 编码规则：活动类型（1位）+日期（首次发现活动的日期）8位+序号（2位）
 		 */
-	    // 0 代表前面补充0         
-	    // 2 代表长度为2         
-	    // d 代表参数为正数型         
-	    String serialStr = String.format("%02d", environmentActitivitySummaryRepo.count()%100); 
+		// 0 代表前面补充0
+		// 2 代表长度为2
+		// d 代表参数为正数型
+		String serialStr = String.format("%02d", environmentActitivitySummaryRepo.count() % 100);
 		String actNo = activity.getActType().substring(0, 1) + format.format(new Date()) + serialStr;
 		activity.setActNo(actNo);
 		activitySummary.setActNo(actNo);
-		
-		if(isBlank(activitySummary.getTSurrInfo().getSurroundNo())){
+
+		if (activitySummary.getTSurrInfo() == null || isBlank(activitySummary.getTSurrInfo().getSurroundNo())) {
 			activitySummary.setTSurrInfo(null);
-		}else{
+		} else {
 			activitySummary.setTSurrInfo(surrInfoRepo.getOne(activitySummary.getTSurrInfo().getSurroundNo()));
 		}
-		
+
 		activitySummary = environmentActitivitySummaryRepo.saveAndFlush(vo.getEnvironmentActitivitySummary());
 		activity = environmentActitivityRepo.saveAndFlush(vo.getEnvironmentActivity());
-		
+
 		vo.setEnvironmentActitivitySummary(activitySummary);
 		vo.setEnvironmentActivity(activity);
-		
+
 		return vo;
 	}
 
