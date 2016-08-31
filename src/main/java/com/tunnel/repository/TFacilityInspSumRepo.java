e530553e6e054017c9bc0f76ab4cf212062f521e
Wayne Cao <wayne911@live.cn>
2016-08-31 23:43:08 +0800

    change

---
+++ src/main/java/com/tunnel/repository/TFacilityInspSumRepo.java
@@ -0,0 +1,9 @@
+package com.tunnel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tunnel.model.TFacilityInspSum;
+
+public interface TFacilityInspSumRepo extends JpaRepository<TFacilityInspSum, String> {
+
+}
--- src/main/java/com/tunnel/web/controller/FacilityInspController.java
+++ src/main/java/com/tunnel/web/controller/FacilityInspController.java
@@ -1,21 +1,55 @@
 package com.tunnel.web.controller;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tunnel.model.TFacilityInspDetail;
+import com.tunnel.model.TFacilityInspSum;
+import com.tunnel.model.TSurrAct;
 import com.tunnel.repository.FacilityInspDetailRepo;
+import com.tunnel.repository.TFacilityInspSumRepo;
+import com.tunnel.vo.EnvironmentActivitiesSummaryVo;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
-@RequestMapping("/facility-insp")
+@RequestMapping
 public class FacilityInspController extends BaseController{
 	
+	@Autowired
 	private FacilityInspDetailRepo facilityInspDetailRepo;
+	
+	@Autowired
+	private TFacilityInspSumRepo facilityInspSumRepo;
 
+	@ApiOperation("给出当前巡检表数据最大ID值")
+	@RequestMapping(value = "/facility-insp/getMaxFacilityInspDetailId", method = RequestMethod.GET)
 	public long getMaxFacilityInspDetailId(){
-		return facilityInspDetailRepo.count();
+		return 1;//max id number
 	}
 	
+	@ApiOperation("列出一页地下巡检详细信息")
+	@RequestMapping(value = "/facility-insp-detail/list", method = RequestMethod.GET)
+	public Page<TFacilityInspDetail> getFacilityInspDetailPage(
+			@PageableDefault(value = 10, sort = { "id" }, direction = Direction.DESC) Pageable pageable) {
+		return facilityInspDetailRepo.findAll(pageable);
+	}
+	
+	@ApiOperation("列出一页地下巡检汇总信息")
+	@RequestMapping(value = "/facility-insp-summary/list", method = RequestMethod.GET)
+	public Page<TFacilityInspSum> getFacilityInspSumPage(
+			@PageableDefault(value = 10, sort = { "id" }, direction = Direction.DESC) Pageable pageable) {
+		return facilityInspSumRepo.findAll(pageable);
+	}
 }
