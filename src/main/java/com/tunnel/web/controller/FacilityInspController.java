650b097b69578194cc6451f4ab154a19899ea225
Wayne Cao <wayne911@live.cn>
2016-09-12 13:55:12 +0800

    明细限制最新前5条

--- src/main/java/com/tunnel/web/controller/FacilityInspController.java
+++ src/main/java/com/tunnel/web/controller/FacilityInspController.java
@@ -43,40 +43,40 @@ public class FacilityInspController extends BaseController {
 	@Autowired
 	private TFacilityInspSumRepo facilityInspSumRepo;
 
-//	@Autowired
-//	private MonomerRepo monomerRepo;
-//
-//	@Autowired
-//	private FacilityRepo facilityRepo;
-//
-//	@Autowired
-//	private FacilityTypeRepo facilityTypeRepo;
-//
-//	@Autowired
-//	private PosDespListRepo posDespListRepo;
-//
-//	@Autowired
-//	private ModelRepo modelRepo;
-//
-//	@Autowired
-//	private DiseaseTypeRepo diseaseTypeRepo;
+	// @Autowired
+	// private MonomerRepo monomerRepo;
+	//
+	// @Autowired
+	// private FacilityRepo facilityRepo;
+	//
+	// @Autowired
+	// private FacilityTypeRepo facilityTypeRepo;
+	//
+	// @Autowired
+	// private PosDespListRepo posDespListRepo;
+	//
+	// @Autowired
+	// private ModelRepo modelRepo;
+	//
+	// @Autowired
+	// private DiseaseTypeRepo diseaseTypeRepo;
 
 	private FacilityInspDetail saveFacilityInspDetail(FacilityInspDetailVo detail) {
 		FacilityInspDetail detailEntity = mapper.map(detail, FacilityInspDetail.class);
 		detailEntity.setId(null);
-//		String id = detailEntity.getDetailType().getId();
-//		if (detailEntity.getDetailType() != null && isNotBlank(id)) {
-//			detailEntity.setDetailType(diseaseTypeRepo.findById(id).orElse(null));
-//		} else {
-//			detailEntity.setDetailType(null);
-//		}
-//
-//		id = detailEntity.getDiseaseType().getId();
-//		if (detailEntity.getDiseaseType() != null && isNotBlank(id)) {
-//			detailEntity.setDiseaseType(diseaseTypeRepo.findById(id).orElse(null));
-//		} else {
-//			detailEntity.setDiseaseType(null);
-//		}
+		// String id = detailEntity.getDetailType().getId();
+		// if (detailEntity.getDetailType() != null && isNotBlank(id)) {
+		// detailEntity.setDetailType(diseaseTypeRepo.findById(id).orElse(null));
+		// } else {
+		// detailEntity.setDetailType(null);
+		// }
+		//
+		// id = detailEntity.getDiseaseType().getId();
+		// if (detailEntity.getDiseaseType() != null && isNotBlank(id)) {
+		// detailEntity.setDiseaseType(diseaseTypeRepo.findById(id).orElse(null));
+		// } else {
+		// detailEntity.setDiseaseType(null);
+		// }
 
 		detailEntity = facilityInspDetailRepo.save(detailEntity);
 		return detailEntity;
@@ -94,72 +94,73 @@ public class FacilityInspController extends BaseController {
 		sumEntity.setPhoto(sum.getPhoto());
 		sumEntity.setRecorder(sum.getRecorder());
 		sumEntity.setWidth(sum.getWidth());
-//		String id = sumEntity.getDetailType().getId();
-//		if (sumEntity.getDetailType() != null && isNotBlank(id)) {
-//			sumEntity.setDetailType(diseaseTypeRepo.findById(id).orElse(null));
-//		} else {
-//			sumEntity.setDetailType(null);
-//		}
-//
-//		id = sumEntity.getDiseaseType().getId();
-//		if (sumEntity.getDiseaseType() != null && isNotBlank(id)) {
-//			sumEntity.setDiseaseType(diseaseTypeRepo.findById(id).orElse(null));
-//		} else {
-//			sumEntity.setDiseaseType(null);
-//		}
+		// String id = sumEntity.getDetailType().getId();
+		// if (sumEntity.getDetailType() != null && isNotBlank(id)) {
+		// sumEntity.setDetailType(diseaseTypeRepo.findById(id).orElse(null));
+		// } else {
+		// sumEntity.setDetailType(null);
+		// }
+		//
+		// id = sumEntity.getDiseaseType().getId();
+		// if (sumEntity.getDiseaseType() != null && isNotBlank(id)) {
+		// sumEntity.setDiseaseType(diseaseTypeRepo.findById(id).orElse(null));
+		// } else {
+		// sumEntity.setDiseaseType(null);
+		// }
 		return facilityInspSumRepo.save(sumEntity);
 	}
 
 	private FacilityInspSum saveFacilityInspSum(FacilityInspSumVo sum) {
 		FacilityInspSum sumEntity = mapper.map(sum, FacilityInspSum.class);
-//		String id = sumEntity.getMonomer().getId();
-//		if (sumEntity.getMonomer() != null && isNotBlank(id)) {
-//			sumEntity.setMonomer(monomerRepo.findById(id).orElse(null));
-//		} else {
-//			sumEntity.setMonomer(null);
-//		}
-//
-//		id = sumEntity.getFacility().getId();
-//		if (sumEntity.getFacility() != null && isNotBlank(id)) {
-//			sumEntity.setFacility(facilityRepo.findById(id).orElseThrow(() -> new RuntimeException("找不到这个设施号")));
-//		} else {
-//			throw new RuntimeException("找不到这个设施号");
-//		}
-//
-//		id = sumEntity.getFacilityType().getId();
-//		if (sumEntity.getFacilityType() != null && isNotBlank(id)) {
-//			sumEntity.setFacilityType(facilityTypeRepo.findById(id).orElse(null));
-//		} else {
-//			sumEntity.setFacilityType(null);
-//		}
-//
-//		id = sumEntity.getPosDespList().getId();
-//		if (sumEntity.getPosDespList() != null && isNotBlank(id)) {
-//			sumEntity.setPosDespList(posDespListRepo.findById(id).orElse(null));
-//		} else {
-//			sumEntity.setPosDespList(null);
-//		}
-//
-//		id = sumEntity.getModelNameList().getId();
-//		if (sumEntity.getModelNameList() != null && isNotBlank(id)) {
-//			sumEntity.setModelNameList(modelRepo.findById(id).orElse(null));
-//		} else {
-//			sumEntity.setModelNameList(null);
-//		}
-//
-//		id = sumEntity.getDetailType().getId();
-//		if (sumEntity.getDetailType() != null && isNotBlank(id)) {
-//			sumEntity.setDetailType(diseaseTypeRepo.findById(id).orElse(null));
-//		} else {
-//			sumEntity.setDetailType(null);
-//		}
-//
-//		id = sumEntity.getDiseaseType().getId();
-//		if (sumEntity.getDiseaseType() != null && isNotBlank(id)) {
-//			sumEntity.setDiseaseType(diseaseTypeRepo.findById(id).orElse(null));
-//		} else {
-//			sumEntity.setDiseaseType(null);
-//		}
+		// String id = sumEntity.getMonomer().getId();
+		// if (sumEntity.getMonomer() != null && isNotBlank(id)) {
+		// sumEntity.setMonomer(monomerRepo.findById(id).orElse(null));
+		// } else {
+		// sumEntity.setMonomer(null);
+		// }
+		//
+		// id = sumEntity.getFacility().getId();
+		// if (sumEntity.getFacility() != null && isNotBlank(id)) {
+		// sumEntity.setFacility(facilityRepo.findById(id).orElseThrow(() -> new
+		// RuntimeException("找不到这个设施号")));
+		// } else {
+		// throw new RuntimeException("找不到这个设施号");
+		// }
+		//
+		// id = sumEntity.getFacilityType().getId();
+		// if (sumEntity.getFacilityType() != null && isNotBlank(id)) {
+		// sumEntity.setFacilityType(facilityTypeRepo.findById(id).orElse(null));
+		// } else {
+		// sumEntity.setFacilityType(null);
+		// }
+		//
+		// id = sumEntity.getPosDespList().getId();
+		// if (sumEntity.getPosDespList() != null && isNotBlank(id)) {
+		// sumEntity.setPosDespList(posDespListRepo.findById(id).orElse(null));
+		// } else {
+		// sumEntity.setPosDespList(null);
+		// }
+		//
+		// id = sumEntity.getModelNameList().getId();
+		// if (sumEntity.getModelNameList() != null && isNotBlank(id)) {
+		// sumEntity.setModelNameList(modelRepo.findById(id).orElse(null));
+		// } else {
+		// sumEntity.setModelNameList(null);
+		// }
+		//
+		// id = sumEntity.getDetailType().getId();
+		// if (sumEntity.getDetailType() != null && isNotBlank(id)) {
+		// sumEntity.setDetailType(diseaseTypeRepo.findById(id).orElse(null));
+		// } else {
+		// sumEntity.setDetailType(null);
+		// }
+		//
+		// id = sumEntity.getDiseaseType().getId();
+		// if (sumEntity.getDiseaseType() != null && isNotBlank(id)) {
+		// sumEntity.setDiseaseType(diseaseTypeRepo.findById(id).orElse(null));
+		// } else {
+		// sumEntity.setDiseaseType(null);
+		// }
 
 		sumEntity = facilityInspSumRepo.save(sumEntity);
 		return sumEntity;
@@ -183,7 +184,7 @@ public class FacilityInspController extends BaseController {
 	// System.out.println(st.minusMonths(6).toDate());
 	// }
 
-	@ApiOperation("返回前台2年内地下巡检详细信息以及汇总信息")
+	@ApiOperation("返回前台地下巡检详细信息以及汇总信息")
 	@RequestMapping(value = "/facility-insp/download", method = RequestMethod.GET)
 	public List<FacilityInspVo> downloadFacilityInsp() {
 		log.info("enter listFacilityInsp...");
@@ -192,7 +193,7 @@ public class FacilityInspController extends BaseController {
 
 		// 如果数据量太大，则修改oracle 设置
 		// alter system set open_cursors=3000 scope=both;
-		return facilityInspSumRepo.findByCreateDateAfterAndIsFromMobileTrue(sinceDate).map(e -> {
+		return facilityInspSumRepo.findByCreateDateAfterAndIsFromMobileTrue(sinceDate).limit(2999).map(e -> {
 			FacilityInspVo resp = new FacilityInspVo();
 			resp.setFacilityInspSum(mapper.map(e, FacilityInspSumVo.class));
 			resp.setFacilityInspDetailList(
