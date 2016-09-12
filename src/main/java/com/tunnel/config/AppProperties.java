d4f0bd9a5611faa6994f0a8fff7afca910088ef9
Wayne Cao <wayne911@live.cn>
2016-09-12 10:25:38 +0800

    file upload

--- src/main/java/com/tunnel/config/AppProperties.java
+++ src/main/java/com/tunnel/config/AppProperties.java
@@ -5,45 +5,11 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-
 @Component
 @ConfigurationProperties(prefix = "tunnel")
 @Setter
 @Getter
 public class AppProperties {
 
-    private String directory;
-
-    private String kettleUrl;
-
-    private String sparkExecutorUrl;
-
-    private String logFile;
-
-    private String notificationServiceUrl;
-
-    private String userMgmtUrl;
-
-    private String token;
-
-    private String appName;
-
-    private String appLogicalName;
-
-    private String appLink;
-
-    private String hadoopDesPath;
-
-    private String muleURL;
-    
-    private String emailServiceUrl;
-    
-    private String emailPlainCreds;
-
-
-    public String getDocsDirectory(String user) {
-        return directory + File.separator + user + File.separator;
-    }
-
+	private String fileUploadBasePath;
 }
--- src/main/java/com/tunnel/web/controller/FileUploadController.java
+++ src/main/java/com/tunnel/web/controller/FileUploadController.java
@@ -1,11 +1,18 @@
 package com.tunnel.web.controller;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.tunnel.config.AppProperties;
+import com.tunnel.util.WebUtils;
 import com.tunnel.vo.FileUploadReturnVo;
 
 import lombok.extern.slf4j.Slf4j;
@@ -15,10 +22,24 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping
 public class FileUploadController extends BaseController {
 
+	@Autowired
+	private AppProperties appProperties;
+
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
 	public FileUploadReturnVo upload(@RequestParam("file") MultipartFile file) throws Exception {
 		log.info("uploading file ... ");
 		file.getContentType();
-		return FileUploadReturnVo.builder().success(true).path("").build();
+
+		String relvDir = "/" + WebUtils.loggedInUser().getLoginId() + "/";
+		new File(relvDir).mkdirs();
+
+		String fileRelvPath = relvDir + new SimpleDateFormat("yyyyMMddHHmmss").format(new Date())
+				+ file.getOriginalFilename();
+		File dest = new File(appProperties.getFileUploadBasePath() + fileRelvPath);
+
+		file.transferTo(dest);
+
+		return FileUploadReturnVo.builder().success(true).path(fileRelvPath).build();
 	}
+
 }
